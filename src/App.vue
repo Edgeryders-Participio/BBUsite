e313b1a531328d3e3d989ce45e72c5733ffc340c
skoteskote <jakobskote@hotmail.com>
2020-06-12 19:31:20 +0200

    Updated to sort by bump

--- src/App.vue
+++ src/App.vue
@@ -24,9 +24,9 @@ export default {
 				}
 			);
 			data = await data.json();
-			// sort the data by created_at
+			// sort the data by bumped_at
 			data = data.sort((a, b) =>
-				this.getTimeStamp(a.created_at) - this.getTimeStamp(b.created_at) < 0 ? 1 : -1
+				this.getTimeStamp(a.bumped_at) - this.getTimeStamp(b.bumped_at) < 0 ? 1 : -1
 			);
 			{/* prettier-ignore */}
 			data = await data.map((e) => {
